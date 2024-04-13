60881668c3426c8975cd7b2e1fcda1097b077225
Toby Milner-Gulland <toby@figure.land>
2024-04-13 11:01:43 +0100

    chore: fixed typo in build script

--- build.ts
+++ build.ts
@@ -1,7 +1,7 @@
 import dts from 'bun-plugin-dts'
 
 await Bun.build({
-  entrypoints: ['./src/index.ts', './src/style.ts', './src/vector2.ts', './src/matrix2d.ts'],
+  entrypoints: ['./src/index.ts', './src/style.ts', './src/vector2.ts', './src/matrix2D.ts'],
   outdir: './dist',
   minify: false,
   plugins: [dts()]
