753d137750b2f307b69b725cf1fe67de9dea500a
Toby Milner-Gulland <toby@figure.land>
2024-04-15 11:39:14 +0100

    feat: added elementToBox

--- src/style.ts
+++ src/style.ts
@@ -1,4 +1,6 @@
-import { Matrix2D, Vector2 } from './api'
+/// <reference lib="dom" />
+
+import { Box, Matrix2D, Vector2 } from './api'
 
 export const transform = (matrix: Matrix2D) =>
   `matrix(${matrix[0]}, ${matrix[1]}, ${matrix[2]}, ${matrix[3]}, ${matrix[4]}, ${matrix[5]})`
@@ -6,3 +8,13 @@ export const transform = (matrix: Matrix2D) =>
 export const scale = (s: number) => transform([s, 0, 0, s, 0, 0])
 
 export const translate = (v: Vector2) => transform([1, 0, 0, 1, v.x, v.y])
+
+export const boxFromElement = (element: HTMLElement): Box => {
+  const { top: y, left: x, width, height } = element.getBoundingClientRect()
+  return {
+    x,
+    y,
+    width,
+    height
+  }
+}
