54927ab880bb9485778f4bbe2b6ecedf5171b370
Toby Milner-Gulland <toby@figure.land>
2024-04-29 12:14:24 +0100

    feat: added geometry+randomess helpers

--- src/random.ts
+++ src/random.ts
@@ -1,4 +1,7 @@
-import { floor } from './number'
+import { cos, floor, sin } from '@figureland/mathkit/number'
+import { vector2 } from '@figureland/mathkit/vector2'
+import { TAU } from '@figureland/mathkit'
+import type { Box, Vector2 } from './api'
 
 type Random = () => number
 
@@ -19,3 +22,46 @@ export const randomElement = <T>(arr: T[], r: Random = random): T =>
 export const randomize = <T>(arr: T[], r: Random = random): T[] => arr.sort(() => r() - 0.5)
 
 export const randomizeInPlace = <T>(arr: T[], r: Random = random) => arr.sort(() => r() - 0.5)
+
+export const randomVector2 = (min: number, max: number, r: Random = random) =>
+  vector2(randomFloat(min, max, r), randomFloat(min, max, r))
+
+export const randomVector2InBox = (box: Box, r: Random = random) =>
+  vector2(randomFloat(box.x, box.x + box.width, r), randomFloat(box.y, box.y + box.height, r))
+
+export const randomVector2InCircle = (radius: number, r: Random = random) => {
+  const angle = randomFloat(0, TAU, r)
+  return vector2(radius * cos(angle), radius * sin(angle))
+}
+
+export const randomVector2InEllipse = (rx: number, ry: number, r: Random = random) => {
+  const angle = randomFloat(0, TAU, r)
+  return vector2(rx * cos(angle), ry * sin(angle))
+}
+
+export const randomVector2InRing = (
+  innerRadius: number,
+  outerRadius: number,
+  r: Random = random
+) => {
+  const angle = randomFloat(0, TAU, r)
+  const radius = randomFloat(innerRadius, outerRadius, r)
+  return vector2(radius * cos(angle), radius * sin(angle))
+}
+
+export const randomVector2InSector = (
+  radius: number,
+  minAngle: number,
+  maxAngle: number,
+  r: Random = random
+) => {
+  const angle = randomFloat(minAngle, maxAngle, r)
+  return vector2(radius * cos(angle), radius * sin(angle))
+}
+
+export const randomVector2InTriangle = (a: Vector2, b: Vector2, c: Vector2, r: Random = random) => {
+  const u = randomFloat(0, 1, r)
+  const v = randomFloat(0, 1 - u, r)
+
+  return vector2(a.x + u * (b.x - a.x) + v * (c.x - a.x), a.y + u * (b.y - a.y) + v * (c.y - a.y))
+}
