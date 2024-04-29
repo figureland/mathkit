73185df22ac1d5a73bbb5e40b8f15c910d3dae72
Toby Milner-Gulland <toby@figure.land>
2024-04-29 12:03:36 +0100

    feat: added constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,17 @@
 export const EPS = 0.000001
 
-export const { PI } = Math
+export const { PI, SQRT2, SQRT1_2 } = Math
+
+export const { EPSILON } = Number
+
+export const TAU = 2 * PI
+
+export const HALF_PI = PI / 2
+
+export const QUARTER_PI = PI / 4
+
+export const DEG_TO_RAD = PI / 180
+
+export const RAD_TO_DEG = 180 / PI
+
+export const GOLDEN_RATIO = 1.61803398875
--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,19 @@ export {
   isNumber
 } from './number'
 
+export {
+  PI,
+  SQRT2,
+  SQRT1_2,
+  EPSILON,
+  TAU,
+  HALF_PI,
+  QUARTER_PI,
+  DEG_TO_RAD,
+  RAD_TO_DEG,
+  GOLDEN_RATIO
+} from './constants'
+
 export type { Vector2 } from './vector2'
 export type { Matrix2D } from './matrix2D'
 export * from './api'
