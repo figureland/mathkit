480a6de2fc332e98eab7e70b89bfbb3c50bb45ff
Toby Milner-Gulland <toby@figure.land>
2024-04-29 12:28:36 +0100

    feat: removed array mutation

--- src/random.ts
+++ src/random.ts
@@ -19,9 +19,10 @@ export const randomSign = (r: Random = random) => (r() >= 0.5 ? 1 : -1)
 export const randomElement = <T>(arr: T[], r: Random = random): T =>
   arr[randomInt(0, arr.length - 1)]
 
-export const randomize = <T>(arr: T[], r: Random = random): T[] => arr.sort(() => r() - 0.5)
+export const randomize = <T>(arr: T[], r: Random = random): T[] => [...arr].sort(() => r() - 0.5)
 
-export const randomizeInPlace = <T>(arr: T[], r: Random = random) => arr.sort(() => r() - 0.5)
+export const randomizeInPlace = <T>(arr: T[], r: Random = random): T[] =>
+  [...arr].sort(() => r() - 0.5)
 
 export const randomVector2 = (min: number, max: number, r: Random = random) =>
   vector2(randomFloat(min, max, r), randomFloat(min, max, r))
